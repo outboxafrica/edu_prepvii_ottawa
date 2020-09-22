20b7e5acad24a5a581a941b2cc161dfe1c274490
Melodiean <melodynsita@gmail.com>
2020-09-22 18:27:31 +0300

    minchanges

--- cart.js
+++ cart.js
@@ -2,29 +2,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let qtys = [...document.querySelectorAll(".qty")]; //quantity
 
-    let prices = document.querySelectorAll(".price"); //prices
+    let prices = [...document.querySelectorAll(".price")]; //prices
 
-    let amt = document.querySelectorAll(".amount"); //amount of a product
+    let amt = [...document.querySelectorAll(".amount")]; //amount of a product
 
-    let tot = document.querySelector("#ttot"); // total
+    let total = document.querySelector("#ttot"); // total
 
     let roo = [...document.querySelectorAll(".pdt_row")]; // product info row
 
     // calculations
-
-      let totarr = [];
+    
     let div_len = roo.length;
+let totarr = [],f_tot;
 
-    for (let it = 0; it < div_len; it++) {
-    let qty1 = parseInt(qtys[it].innerHTML);
-    let prices1 = parseInt(prices[it].innerHTML);
-    let amt1 = parseInt(amt[it].innerHTML);
 
-    amt1 += amt1;
-    console.log(amt1[it]);
+    for(let i=0; i<div_len;i++){
+        totarr.push(parseInt(amt[i].innerHTML));
+    }
+    f_tot = totarr.reduce(( acc, cur ) => acc + cur,
+    0
+  );
+
+  total.textContent = f_tot;
 
     minus = function () {
       let count = 0;
+      for (let it = 0; it < div_len; it++) {
+        let qty1 = parseInt(qtys[it].innerHTML);
+        let prices1 = parseInt(prices[it].innerHTML);
+        let amt1 = parseInt(amt[it].innerHTML);
+        let tot = parseInt(total.innerHTML);
       if (true) {
         count++;
         qty1 -= count;
@@ -32,14 +39,18 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       qtys[it].textContent = qty1;
       amt[it].textContent = amt1;
-      tot.textContent = parseInt(amt1 + tot);
-
-      // return
-      console.log(qty1 + " " + prices1 + " " + amt1);
+      tot +=amt1;
+      total.textContent = tot;
+    }
     };
 
     plus = function () {
       let count = 0;
+      for (let it = 0; it < div_len; it++) {
+        let qty1 = parseInt(qtys[it].innerHTML);
+        let prices1 = parseInt(prices[it].innerHTML);
+        let amt1 = parseInt(amt[it].innerHTML);
+        let tot = parseInt(total.innerHTML);
       if (true) {
         count++;
         qty1 += count;
@@ -47,32 +58,31 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       qtys[it].textContent = qty1;
       amt[it].textContent = amt1;
-      tot.textContent = parseInt(amt1 + tot);
-      console.log(qty1 + " " + prices1 + " " + amt1);
-      
-    };
-}
+      tot +=amt1; 
+      console.log(tot);
+      total.textContent = tot;
+    }
+};
   });
 
-
    /* cart figures */
-   var pdts = [...document.querySelectorAll(".pdt_row")];
+    var pdts = [...document.querySelectorAll(".pdt_row")];
     let can = document.querySelector("#cart_num");
 
-   can.textContent = pdts.length; 
+    can.textContent = pdts.length; 
 
 /* Remove an item from the cart */
    function remov(btn) {
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
+    // console.log(row);
     
     pdts = [...document.querySelectorAll(".pdt_row")];
     let rowCount = pdts.length;
-    console.log(rowCount);
+    // console.log(rowCount);
     can.textContent = pdts.length;
     if (rowCount == 0) {
         window.location = "home.html";
       }
-            
         }  
       
