14cdda5007cb9ef7ccb467097b9f1173d860bdaf
NagwataEzekiel <nagwataezekiel@gmail.com>
2020-09-24 13:44:36 -0800

    new changes

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-var objPeople = [
+/*var objPeople = [
     {
       useremail: "melon1@gmail.com",
       password: "123",
@@ -31,5 +31,36 @@ var objPeople = [
     }
     console.log("incorrect useremail or password");
     alert("invalid useremail or password");
+  }*/
+
+  // creating validation variables
+function validation() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var error_message = document.getElementById("error_message");
+  var text;
+  error_message.style.padding = "10px";
+
+  // user validation starts here
+  if (email.indexOf("@") == -1 || email.length < 6 || email == "" || email == null) {
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
   }
+  if (password.length != 8  ) {
+    text = "Password must be 8 characters or more";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (validation() == true) {
+    let user = {
+      user_email: "email",
+      user_password: "password",
+    };
+    window.localStorage.setItem("person", JSON.stringify(user));
+  }
+  window.open("home.html", "_self");
+}
+// validation ends here
+
    
