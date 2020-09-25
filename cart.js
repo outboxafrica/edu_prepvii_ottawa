ab1b958d05a501bb575c3782c06ee6d58b514bd6
Melodiean <melodynsita@gmail.com>
2020-09-25 16:24:07 +0300

    -js -css files

--- cart.js
+++ cart.js
@@ -1,86 +1,95 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", Total);
 
     let qtys = [...document.querySelectorAll(".qty")]; //quantity
 
     let prices = [...document.querySelectorAll(".price")]; //prices
 
-    let amt = [...document.querySelectorAll(".amount")]; //amount of a product
-
-    let total = document.querySelector("#ttot"); // total
-
     let roo = [...document.querySelectorAll(".pdt_row")]; // product info row
 
     // calculations
-    
     let div_len = roo.length;
-let totarr = [],f_tot;
 
+    // minus = function () {
+    //   let count = 0;
+    //   for (let it = 0; it < div_len; it++) {
+    //     let qty1 = parseInt(qtys[it].innerHTML);
+    //     let prices1 = parseInt(prices[it].innerHTML);
+    //     let amt1 = parseInt(amt[it].innerHTML);
+    //     let tot = parseInt(total.innerHTML);
+    //   if (true) {
+    //     count++;
+    //     qty1 -= count;
+    //     amt1 = qty1 * prices1;
+    //   }
+    //   qtys[it].textContent = qty1;
+    //   amt[it].textContent = amt1;
+    //   tot +=amt1;
+    //   total.textContent = tot;
+    // }
+    // };
 
-    for(let i=0; i<div_len;i++){
-        totarr.push(parseInt(amt[i].innerHTML));
-    }
-    f_tot = totarr.reduce(( acc, cur ) => acc + cur,
-    0
-  );
+//     plus = function () {
+//       let count = 0;
+//       for (let it = 0; it < div_len; it++) {
+//         let qty1 = parseInt(qtys[it].innerHTML);
+//         let prices1 = parseInt(prices[it].innerHTML);
+//         let amt1 = parseInt(amt[it].innerHTML);
+//         let tot = parseInt(total.innerHTML);
+//       if (true) {
+//         count++;
+//         qty1 += count;
+//         amt1 = qty1 * prices1;
+//       }
+//       qtys[it].textContent = qty1;
+//       amt[it].textContent = amt1;
+//       tot +=amt1; 
+//       total.textContent = tot;
+//     }
+// };
 
-  total.textContent = f_tot;
+// });
 
-    minus = function () {
-      let count = 0;
-      for (let it = 0; it < div_len; it++) {
-        let qty1 = parseInt(qtys[it].innerHTML);
-        let prices1 = parseInt(prices[it].innerHTML);
-        let amt1 = parseInt(amt[it].innerHTML);
-        let tot = parseInt(total.innerHTML);
-      if (true) {
-        count++;
-        qty1 -= count;
-        amt1 = qty1 * prices1;
-      }
-      qtys[it].textContent = qty1;
-      amt[it].textContent = amt1;
-      tot +=amt1;
-      total.textContent = tot;
-    }
-    };
+let pdts = [...document.querySelectorAll(".pdt_row")];
+    let total = document.querySelector("#ttot"); // total
 
-    plus = function () {
-      let count = 0;
-      for (let it = 0; it < div_len; it++) {
-        let qty1 = parseInt(qtys[it].innerHTML);
-        let prices1 = parseInt(prices[it].innerHTML);
-        let amt1 = parseInt(amt[it].innerHTML);
-        let tot = parseInt(total.innerHTML);
-      if (true) {
-        count++;
-        qty1 += count;
-        amt1 = qty1 * prices1;
-      }
-      qtys[it].textContent = qty1;
-      amt[it].textContent = amt1;
-      tot +=amt1; 
-      console.log(tot);
-      total.textContent = tot;
-    }
-};
-  });
+function Total(amt) {
+   amt = [...document.querySelectorAll(".amount")]; //amount of a product
+
+  let f_tot = 0;
+   let amta =  amt.map(e=>{
+     return parseInt(e.innerHTML);
+   });
+
+    f_tot = amta.reduce(( acc, cur ) => acc + cur,
+        0
+      );
+      total.textContent = f_tot;
+      return f_tot;
+}
 
    /* cart figures */
-    var pdts = [...document.querySelectorAll(".pdt_row")];
     let can = document.querySelector("#cart_num");
-
-    can.textContent = pdts.length; 
+    can.textContent = pdts.length;
 
 /* Remove an item from the cart */
    function remov(btn) {
+    
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
-    
     pdts = [...document.querySelectorAll(".pdt_row")];
+    amt = [...document.querySelectorAll(".amount")]
+
+    let tota = Total(amt);
+
     let rowCount = pdts.length;
     can.textContent = pdts.length;
+
+    total.textContent = tota;
+
     if (rowCount == 0) {
         window.location = "home.html";
       }
         }  
         
+
+     
