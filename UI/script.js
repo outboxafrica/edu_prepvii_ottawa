f96bfbc79fa4c7131cb1957d89fd3a9d0b73583e
wamani wills <wamaniwillcomfort@gmail.com>
2020-09-27 00:00:01 +0300

    organised product section

--- UI/script.js
+++ UI/script.js
@@ -49,4 +49,22 @@ function myFunc(){
 };
 
 
-})
+});
+ /* cart figures */
+ var pdts = [...document.querySelectorAll(".pdt_row")];
+ let can = document.querySelector("#cart_num");
+
+ can.textContent = pdts.length; 
+
+/* Remove an item from the cart */
+function remov(btn) {
+ var row = btn.parentNode.parentNode;
+ row.parentNode.removeChild(row);
+ 
+ pdts = [...document.querySelectorAll(".pdt_row")];
+ let rowCount = pdts.length;
+ can.textContent = pdts.length;
+ if (rowCount == 0) {
+     window.location = "home.html";
+   }
+     }  
